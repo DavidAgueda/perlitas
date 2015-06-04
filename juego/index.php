edc7e07b35e9297e9741191aa8f29f5dff8b285e
dagueda <dagueda@192.168.1.189>
2015-06-04 15:24:46 +0200

    juego v0.1

--- juego/index.php
+++ juego/index.php
@@ -10,12 +10,11 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <h1>Cosas interesantes que aprendo</h1>
+        <p>Poner un cuadrado posicionarlo con css en el medio y moverlo con las flechas</p>
+        <p>Cada vez que pulsemos una fecha envia un requete por ajax con las coordenadas</p>
+        <p>poner un segundo cuadrado que se mueva desde otra web</p>
         <?php
         // put your code here
         ?>
-        <ul>
-            <li><a href="juego/index.php">juego</a></li>
-        </ul>
     </body>
 </html>
